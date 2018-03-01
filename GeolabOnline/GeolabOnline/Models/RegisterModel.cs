ec888b7be5ce73e29b47c4ed39564117cef490e9
fidoosiashvili <fridon.osiashvili@geolab.edu.ge>
2018-03-01 23:37:39 +0400

    baza/avtorizacis da registracis serviesebi

---
+++ GeolabOnline/GeolabOnline/Models/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GeolabOnline.Models
+{
+    public class RegisterModel
+    {
+        public string  UserName { get; set; }
+        public string  Password { get; set; }
+        public string PassowrdCon { get; set; }
+        public string Mail { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreateDate = DateTime.Now;
+    }
+}
